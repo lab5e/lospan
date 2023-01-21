15e263a568cec42bfb75b1f2949b8df8f27996e6
Ståle Dahl <stalehd@gmail.com>
2023-01-21 15:57:00 +0100

    Set default endpoint for lora api in client

--- cmd/lc/params.go
+++ cmd/lc/params.go
@@ -1,7 +1,7 @@
 package main
 
 type params struct {
-	Address string    `kong:"help='Address of lora server API',default='127.0.0.1:4711'"`
+	Address string    `kong:"help='Address of lora server API',default='127.0.0.1:5150'"`
 	App     appCmd    `kong:"cmd,help='Application commands',aliases='application,a'"`
 	Dev     devCmd    `kong:"cmd,help='Device commands',aliases='device,d'"`
 	GW      gwCmds    `kong:"cmd,help='Gateway commands',aliases='gateway,g'"`
